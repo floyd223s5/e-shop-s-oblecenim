f5449f31d26f729ecaf04f1a3d473832c402caf4
floyd223s5 <161425738+floyd223s5@users.noreply.github.com>
2025-01-17 02:22:57 +0100

    Update

--- src/views/SweatshirtsView.vue
+++ src/views/SweatshirtsView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container mx-auto px-6 py-12">
-    <h1 class="text-3xl font-bold text-center text-gray-900 mb-8">Hoodies</h1>
+    <h1 class="text-3xl font-bold text-center text-gray-900 mb-8">Sweatshirts</h1>
     <p class="text-center text-gray-600 mb-8">
-      Browse our exclusive collection of hoodies, crafted for comfort and style.
+      Browse our exclusive collection of Sweatshirts, crafted for comfort and style.
     </p>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
       <ProductCard v-for="product in filteredProducts" :key="product.id" :product="product" />
